c5a639eccf52ce51c692d07752f599b8d16cacc1
tanpoya <tanpoya02@gmail.com>
2022-10-28 17:20:22 +0900

    1

--- project/js/script.js
+++ project/js/script.js
@@ -1,10 +1,24 @@
-window.addEventListener("DOMContentLoaded", ()=>{
-    const rimg = document.querySelectorAll(".intro_list_tbox_img img");
+window.addEventListener("DOMContentLoaded", function(){
 
-    // console.log(rimg);
 
-    setInterval(() => {
-        
-    }, 3000);
+});
 
-})
+/*
+
+1. intro_list_tbox_img 이미지
+
+1) 대상: index.html 102번째 이미지 3개
+2) 이벤트: fade in/out
+3) 이벤트시: 세개의 이미지들이 순서대로
+            1.5초 간격으로 보임(무한루프)
+
+2. abox_img 처리
+
+1) 대상: abox_img
+2) 이벤트: abox 마우스오버
+3) 이벤트시: abox_img에 오버시 이미지가 위에서 중앙으로 내려왔다가
+            아웃시 중앙에서 밑으로 사라짐
+            (각 박스마다 이미지가 다름)
+
+
+*/
