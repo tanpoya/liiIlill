e0fdc81dd987fb3e281a13dfc6c612d5892ecd5f
tanpoya <tanpoya02@gmail.com>
2022-11-04 17:19:23 +0900

    1

--- project/js/script.js
+++ project/js/script.js
@@ -15,7 +15,7 @@ $(() => {
                                 .delay(1500)
                                 .fadeIn(2000, () =>{ 
                                     // 기존 style값을 모두 없앰!
-                                    rimg.attr("style","");
+                                    rimg.attr("style", "");
                                     // 자기자신함수 다시호출
                                     fadeImg();
                                 });
@@ -24,51 +24,48 @@ $(() => {
     };
     fadeImg();
 
-    
+    const abox = $(".abox");
+    const iimg = $(".abox_iimg");
+
+    console.log(abox);
+    console.log(iimg);
+
+    /* 
+        abox 오버시 각 abox에 해당하는 iimg가
+        위에서 중간으로 멈췄다가 아웃시 내려감
+    */
+   
+    $(abox[0]).mouseover(function() {
+        $(iimg[0]).css({
+            transform: "translateY(0)",
+            transition: "transform 0.5s ease-out",
+        }).removeClass("on");
+    });
+    $(abox[0]).mouseout(function() {
+        $(iimg[0]).css({
+            transform: "translateY(100vh)",
+        }).addClass("on");
+    });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// <<<<<<< HEAD
-    
-
-
-
-    
-    
-// =======
-// >>>>>>> 5dcd16ccd250fe12908a5dfcc9f94ffdd377174f
 });
 
 /*
 
-1. intro_list_tbox_img 이미지
+1. intro_list_tbox_img 이미지 ** 컷
 
 1) 대상: index.html 102번째 이미지 3개 (intro_list_tbox_img .rimg #rimg 1 2 3)
 2) 이벤트: fade in/out
 3) 이벤트시: 세개의 이미지들이 순서대로
-1.5초 간격으로 사라짐(무한루프)
 
-2. abox_img 처리
+2. abox_img 처리 * 진행중
 
 1) 대상: abox_img
 2) 이벤트: abox 마우스오버
 3) 이벤트시: abox_img에 오버시 이미지가 위에서 중앙으로 내려왔다가
 아웃시 중앙에서 밑으로 사라짐
 (각 박스마다 이미지가 다름)
+
 
 
 */
