cc70fe7c4643ef4c02b86b4e3222cf01d764c57e
tanpoya <tanpoya02@gmail.com>
2022-11-08 17:19:40 +0900

    1

--- project/js/script.js
+++ project/js/script.js
@@ -1,21 +1,20 @@
 $(() => {
     const rimg = $(".rimg");
-    console.log(rimg);
+    // console.log(rimg);
 
     const fadeImg = () => {
         console.log("들어옴~!");
-        $(rimg[2])
-            .delay(1500)
-            .fadeOut(2000, function () {
-                $(rimg[1])
-                    .delay(1500)
-                    .fadeOut(2000, function () {
+        $(rimg[2]).delay(1500).fadeOut(2000, function () {
+
+                $(rimg[1]).delay(1500).fadeOut(2000, function () {
+
                         $(rimg[0]).delay(1500).fadeOut(2000),
-                            $(rimg[2])
-                                .delay(1500)
-                                .fadeIn(2000, () =>{ 
+
+                            $(rimg[2]).delay(1500).fadeIn(2000, () =>{ 
+
                                     // 기존 style값을 모두 없앰!
                                     rimg.attr("style", "");
+
                                     // 자기자신함수 다시호출
                                     fadeImg();
                                 });
@@ -24,14 +23,44 @@ $(() => {
     };
     fadeImg();
 
+   /*  // 변수셋팅 순번변수
+    let sno = 0;
+    // 인터발용 변수
+    let autoI;
+    // 타임아웃용 변수
+    let autoT;
+
+    function sildeAuto() {
+        autoI = setInterval(() => {
+            // 순번증가
+            sno++;
+
+            // 한계값체크
+            if(sno === rimg.length) sno = 0;
+
+            // 해당순번(sno) 클래스(on)넣기
+            rimg.eq(sno).addClass("on").siblings().removeClass("on");
+
+        }, 2500);
+    }
+    sildeAuto();
+    // index.html rimg 하나에 클래스 on
+    // style.css 179~185 주석풀기
+    */
+
+
     const abox = $(".abox");
     const iimg = $(".abox_iimg");
 
-    let abc;
-    const abcp = [];
+    // 스크롤 위치변수
+    let scTop;
+    let lastSc= 0;
+    const scpos = [];
+    let  scNum = $(".counter_box_num");
+    console.log(scNum);
 
-    console.log(abox);
-    console.log(iimg);
+    // console.log(abox);
+    // console.log(iimg);
 
     /* 
         abox 오버시 각 abox에 해당하는 iimg가
@@ -62,11 +91,45 @@ for(let i = 0; i < abox.length; i++) {
 
 
 
-// $(window).scroll(function() {
-//     abc = $(this).scrollTop();
-//     console.log(abc);
-// })
+
+
+// 스크롤 위치 확인
+$(window).scroll(function() {
+    abc = $(this).scrollTop();
+    console.log(abc);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // https://lpla.tistory.com/157
+
+// https://webisfree.com/2014-09-07/[jquery]-%EC%A0%88%EB%8C%80%EC%A2%8C%ED%91%9C-%EB%B0%8F-%EC%83%81%EB%8C%80%EC%A2%8C%ED%91%9C-%ED%99%95%EC%9D%B8-%EB%B0%8F-%EC%9D%B4%EB%8F%99%ED%95%98%EA%B8%B0-offset()-position()
+// let aboxImg = iimg.position().top
+// console.log(aboxImg);
+// https://lpla.tistory.com/157
+
+
+
 
 /*
 
