037e51692eac2d66f287eba288cf88643d69afd3
tanpoya <tanpoya02@gmail.com>
2022-12-07 16:09:14 +0900

    0

--- project/js/script.js
+++ project/js/script.js
@@ -1,6 +1,4 @@
 $(() => {
-    
-    let a = 1;
 
     // 헤더 / 네비 업다운
     let hdud;
@@ -8,15 +6,15 @@ $(() => {
     let delta = 5; // 스크롤이 100마다 움직이는데 5 이상 움직일때
     let navbarHeight = $("header").outerHeight();
     // console.log(st);
-    
-    $(window).scroll(function(e) {
+
+    $(window).scroll(function (e) {
         hdud = true;
         // console.log(e);
         // console.log(hdud);
     });
-    
-    setInterval(function() {
-        if(hdud) {
+
+    setInterval(function () {
+        if (hdud) {
             hdudS();
             hdud = false;
             // console.log(hdud);
@@ -24,20 +22,18 @@ $(() => {
     }, 250);
 
     function hdudS() {
-
         let st = $(this).scrollTop();
         // Math.abs 절대값
         // 0 - 현재위치 <= 5
-        if(Math.abs(lastSc - st) <= delta) return;
+        if (Math.abs(lastSc - st) <= delta) return;
 
-        if(st > lastSc && st > navbarHeight) {
+        if (st > lastSc && st > navbarHeight) {
             $("header").removeClass("nav-down").addClass("nav-up");
         } else {
-                $("header").removeClass("nav-up").addClass("nav-down");
+            $("header").removeClass("nav-up").addClass("nav-down");
         }
         lastSc = st;
     }
-
 
     // startSS();
 
@@ -46,14 +42,16 @@ $(() => {
 
     const fadeImg = () => {
         // console.log("들어옴~!");
-        $(rimg[2]).delay(1500).fadeOut(2000, function () {
-
-                $(rimg[1]).delay(1500).fadeOut(2000, function () {
-
+        $(rimg[2])
+            .delay(1500)
+            .fadeOut(2000, function () {
+                $(rimg[1])
+                    .delay(1500)
+                    .fadeOut(2000, function () {
                         $(rimg[0]).delay(1500).fadeOut(2000),
-
-                            $(rimg[2]).delay(1500).fadeIn(2000, () =>{ 
-
+                            $(rimg[2])
+                                .delay(1500)
+                                .fadeIn(2000, () => {
                                     // 기존 style값을 모두 없앰!
                                     rimg.attr("style", "");
 
@@ -65,7 +63,7 @@ $(() => {
     };
     fadeImg();
 
-   /*  // 변수셋팅 순번변수
+    /*  // 변수셋팅 순번변수
     let sno = 0;
     // 인터발용 변수
     let autoI;
@@ -90,67 +88,94 @@ $(() => {
     // style.css 179~185 주석풀기
     */
 
-
     const abox = $(".abox");
     const iimg = $(".abox_iimg");
 
-    
     // console.log(abox);
     // console.log(iimg);
-    
-    /* 
-    abox 오버시 각 abox에 해당하는 iimg가
-    위에서 중간으로 멈췄다가 아웃시 내려감
-    */
-   for(let i = 0; i < abox.length; i++) {
-    $(abox[i]).mouseover(function() {
-        $(iimg[i]).css({
-            transform: "translateY(0)",
-            transition: "transform 0.5s ease-out",
-        })
-        // iimg.each((idx, ele) => {
-            //     abcp[idx] = $(ele).offset().top;
-            //     console.log("오버시: ", abcp[idx]);
-            // });
+
+    for (let i = 0; i < abox.length; i++) {
+        $(abox[i]).mouseenter(function () {
+            $(iimg[i]).css({
+                transform: "translateY(0)",
+                transition: "transform 0.5s ease-out",
+            });
+            prota = 1;
+            console.log("over:", prota);
         });
-        $(abox[i]).mouseout(function() {
+        $(abox[i]).mouseleave(function () {
+            prota = 0;
             $(iimg[i]).css({
                 transform: "translateY(100vh)",
-                transition: "transform 0.5s ease-in"
-            })
-            // iimg.each((idx, ele) => {
-                //     abcp[idx] = $(ele).offset().top;
-                //     console.log("아웃시: ", abcp[idx]);
-                // });
+                transition: "transform 0.5s ease-in",
+            },);
+            setTimeout(trasnR, 400);
+        });
+        
+        let prota = 0;
+
+    function trasnR() {
+        if(prota) return;
+        prota = 1;
+        if(prota) {
+            $(iimg[i]).css({
+                transform: "translateY(-100vh)",
+                transition: "none"
             });
-        };
+        }
+        // setTimeout(()=>(prota = 0), 800);
+        console.log("out:", prota);
+    }
+} // for ///
 
-        const i2b_cb = $(".i2b_cb");
-        const buynow = $(".buynow");
-        const buy = $(".buy");
-        const edition_sw = $(".edition-sw");
-        // console.log(i2b_cb);
-        const i2bColor = ["white", "red", "pink", "white", "yellow", "white", "mediumspringgreen", "bisque", "white", "darkred"];
-        const i2bColor2 = ["black", "white", "black", "darkturquoise", "black", "black", "darkblue", "rgb(212, 141, 183)", "rgb(58, 187, 206)", "white"];
 
-        i2bColor.forEach((ele, idx) => {
-            // console.log(ele, idx);
-            $(i2b_cb[idx]).css({
-                color: `${ele}`
-            }), // i2b_cb 색변경
+    const i2b_cb = $(".i2b_cb");
+    const buynow = $(".buynow");
+    const buy = $(".buy");
+    const edition_sw = $(".edition-sw");
+    // console.log(i2b_cb);
+    const i2bColor = [
+        "white",
+        "red",
+        "pink",
+        "white",
+        "yellow",
+        "white",
+        "mediumspringgreen",
+        "bisque",
+        "white",
+        "darkred",
+    ];
+    const i2bColor2 = [
+        "black",
+        "white",
+        "black",
+        "darkturquoise",
+        "black",
+        "black",
+        "darkblue",
+        "rgb(212, 141, 183)",
+        "rgb(58, 187, 206)",
+        "white",
+    ];
+
+    i2bColor.forEach((ele, idx) => {
+        // console.log(ele, idx);
+        $(i2b_cb[idx]).css({
+            color: `${ele}`,
+        }), // i2b_cb 색변경
             $(buynow[idx]).css({
-                backgroundColor: `${ele}`
+                backgroundColor: `${ele}`,
             }), // buynow css
             i2bColor2.forEach((e, i) => {
                 $(buy[i]).css({
-                    color: `${e}`
-                })
+                    color: `${e}`,
+                });
             }),
-                $(edition_sw[idx]).css({
-                    color: `${ele}`
-                })
-        });
-                    
+            $(edition_sw[idx]).css({
+                color: `${ele}`,
+            });
+    });
 });
 
 // i2bColor2.forEach((e, i) => {
@@ -158,12 +183,6 @@ $(() => {
 //         color: `${e}`
 //     })
 // })
-
-
-
-
-
-
 
 // https://lpla.tistory.com/157
 
